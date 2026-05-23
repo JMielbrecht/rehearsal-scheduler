e1c1abd6805958faaea89e40e66eb46bbd3f11c2
Jordan Mielbrecht <jordan.mielbrecht@gmail.com>
2026-05-23 10:13:53 -0700

    import Cast type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,12 @@
-import { Script } from '../types';
+import { Cast, Script } from '../types';
 
-export const createSchedule = (script: Script, ...args) => {
+export const createSchedule = (script: Script, cast: Cast): void => {
   const scenes = script.scenes;
   console.log('scenes: ', scenes);
+
+  for (const scene of scenes) {
+    // TODO: iterate through each scene and get the following: 
+    // - Characters
+    // - Create rehearsal blocks? 
+  }
 }
