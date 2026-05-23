b59076b820577fc4fc7d722b9494128064f2df75
Jordan Mielbrecht <jordan.mielbrecht@gmail.com>
2026-05-23 10:13:34 -0700

    export interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,30 +1,30 @@
 
-interface Person {
+export interface Person {
   name: string;
   role: string;
   conflicts: Date[];
 }
 
-enum RehearsalType {
+export enum RehearsalType {
   Blocking,
   Choreography,
   Music
 }
 
-interface RehearsalBlock {
+export interface RehearsalBlock {
   type: RehearsalType;
   duration: number; // Hours required
 }
 
-interface Scene {
+export interface Scene {
   length: number; //pages
   characters: Array<string>; // corresponds to all valid Person.role (TODO: strongly type? How enforced?)
 }
-interface Script {
+export interface Script {
   length: number; // pages (top-level? or maybe derived from Scenes?)
   scenes: Array<Scene>;
 }
 
-interface Cast {
+export interface Cast {
   castMembers: Array<Person>;
 }
