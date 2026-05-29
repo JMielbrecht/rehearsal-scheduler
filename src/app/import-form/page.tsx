60e6c59cc76d4c41892a0cf63245853d5176dfdf
Jordan Mielbrecht <jordan.mielbrecht@gmail.com>
2026-05-28 22:35:59 -0700

    add basic pages for import form and schedule views

---
+++ src/app/import-form/page.tsx
@@ -0,0 +1,5 @@
+// This is where and how the script and actor information will be imported to be used by the scheduling algorithm.
+
+export default function Page() {
+  return <h1>FORM GOES HERE</h1>
+}
---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,5 @@
+// This component is for the schedule view -- like calendar view with draggable components
+
+export default function Page = () {
+  return <div>This will contain rehearsal blocks. Might be broken up into weekly views, or just one month block for now.</div>
+}
